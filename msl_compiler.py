31a0ff42239d855607b1c0ec034b3b835788b16b
Luna Ruby <lkmnds@gmail.com>
2016-04-07 17:59:30 +1000

    adding the foundations

---
+++ msl_compiler.py
@@ -0,0 +1,19 @@
+import sys
+
+argv = sys.argv
+
+def msl_tokenize(s):
+    i = 0
+    tokens = []
+    while i < len(s):
+        char = s[i]
+    return tokens
+
+def main():
+    if len(argv) > 1
+        with open(argv[1], 'r') as f:
+            p = tokenize(f.readAll())
+            execute(p)
+
+if __name__ == '__main__':
+    main()
