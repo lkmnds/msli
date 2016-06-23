6d06b23cb79d441ed38fb8685f6d45ab114646c1
Luna Mendes <lkmnds@gmail.com>
2016-06-23 19:21:27 +1000

    Add repl-loop

--- src/py/msl.py
+++ src/py/msl.py
@@ -1,9 +1,31 @@
+import traceback
+import readline
 import sys
 argv = sys.argv
 
+def msl_read(string):
+    return string
+
+def msl_eval(ast, env):
+    return ast
+
+def msl_print(exp):
+    return exp
+
+def msl_rep(string):
+    return msl_print(msl_eval(msl_read(string), {}))
 
 def main():
-    pass
+    # repl loop
+    while True:
+        try:
+            line = input("msl> ")
+            if line == None: break
+            if line == "": continue
+
+            print(msl_rep(line))
+        except Exception as e:
+            print("".join(traceback.format_exception(*sys.exc_info())))
 
 if __name__ == '__main__':
     main()
