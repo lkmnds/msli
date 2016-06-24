2de096c6496399aa43e6ea0334fad333c7961a65
Luna Mendes <lkmnds@gmail.com>
2016-06-24 19:28:44 +1000

    fix function calls

--- src/py/msl.py
+++ src/py/msl.py
@@ -2,12 +2,16 @@ import traceback
 import readline
 import sys
 argv = sys.argv
+import os
 
 import msl_reader as reader
 import msl_printer as printer
 
 import msl_types as mtypes
 import msl_env as menv
+
+hist_loaded = False
+hist_file = os.path.expanduser("~/.msl-history")
 
 def msl_read(string):
     return reader.read_str(string)
@@ -96,8 +100,8 @@ def msl_eval(ast, env):
                 else:
                     d = eval_ast(ast, env)
                     fargs = d.values[1:]
-                    envfunc = env.find(funcname.symval)
-                    return envfunc.get(funcname.symval)(*fargs)
+                    envfunc = env.find(funcname)
+                    return envfunc.get(funcname)(*fargs)
         else:
             return eval_ast(ast, env)
     else:
@@ -118,9 +122,22 @@ def msl_rep(string):
 
 def main():
     # repl loop
+    global hist_loaded
     while True:
         try:
+            if not hist_loaded:
+                hist_loaded = True
+                try:
+                    with open(hist_file, 'r') as hf:
+                        for line in hf.readlines():
+                            readline.add_history(line.rstrip("\r\n"))
+                except IOError:
+                    pass
             line = input("msl> ")
+            readline.add_history(line)
+
+            with open(hist_file, 'a') as hf:
+                hf.write(line + '\n')
             if line == None: break
             if line == "": continue
 
