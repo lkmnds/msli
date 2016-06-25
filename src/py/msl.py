c50a487f87170a7f97f36b9f4220ad98cb81cb83
Luna Ruby <lkmnds@gmail.com>
2016-06-25 15:37:37 +1000

    Add TCO

--- src/py/msl.py
+++ src/py/msl.py
@@ -43,93 +43,97 @@ def eval_ast(ast, env):
         return ast
 
 def msl_eval(ast, env):
-    if hasattr(ast, 'type'):
-        if ast.type == 'list':
-            if len(ast) == 0:
-                return ast
-            else:
-                funcname = ast.values[0]
-
-                if hasattr(funcname, 'type'):
-                    if funcname.type == 'symbol':
-                        funcname = funcname.symval
-
-                if funcname == 'def!':
-                    a1, a2 = ast.values[1], ast.values[2]
-                    res = msl_eval(a2, env)
-                    return env.set(a1.symval, res)
-
-                elif funcname == "let*":
-                    a1, a2 = ast.values[1], ast.values[2]
-                    let_env = menv.Enviroment(env)
-
-                    for i in range(0, len(a1), 2):
-                        let_env.set(a1.values[i].symval, msl_eval(a1.values[i+1], let_env))
-                    return msl_eval(a2, let_env)
-
-                elif funcname == 'exit' or funcname == 'quit':
-                    a1 = ast.values[1]
-                    if not isinstance(a1, mtypes.MslNumber):
-                        raise Exception("A number is required")
-                    sys.exit(a1.num)
-
-                elif funcname == 'do':
-                    vals = mtypes.MslList(ast.values[1:])
-                    return eval_ast(vals, env).values[-1]
-
-                elif funcname == 'if':
-                    a1 = ast.values[1]
-                    ret = msl_eval(a1, env)
-
-                    true_condition = ast.values[2]
-                    false_condition = None
-                    if len(ast.values) > 3:
-                        false_condition = ast.values[3]
-
-                    print(bool(ret))
-
-                    if ret:
-                        return msl_eval(true_condition, env)
-                    else:
-                        if false_condition:
-                            return msl_eval(false_condition, env)
-                        else:
-                            return None
-
-                elif funcname == 'fn*':
-                    a1, a2 = ast.values[1], ast.values[2]
-                    return mtypes.MslFunction(msl_eval, menv.Enviroment, a2, env, a1)
-
+    while True:
+        if hasattr(ast, 'type'):
+            if ast.type == 'list':
+                if len(ast) == 0:
+                    return ast
                 else:
-                    d = eval_ast(ast, env)
-                    fargs = d.values[1:]
-                    envfunc = env.find(funcname)
-                    func_call = None
-                    func_type = ''
+                    funcname = ast.values[0]
 
                     if hasattr(funcname, 'type'):
-                        if funcname.type == 'function':
-                            # function call(maybe?)
-                            func_call = funcname
-                        elif funcname.type == 'list':
-                            # got lambda
-                            func_call = msl_eval(funcname, env)
-                            func_type = 'decl'
+                        if funcname.type == 'symbol':
+                            funcname = funcname.symval
+
+                    if funcname == 'def!':
+                        a1, a2 = ast.values[1], ast.values[2]
+                        res = msl_eval(a2, env)
+                        return env.set(a1.symval, res)
+
+                    elif funcname == "let*":
+                        a1, a2 = ast.values[1], ast.values[2]
+                        let_env = menv.Enviroment(env)
+
+                        for i in range(0, len(a1), 2):
+                            let_env.set(a1.values[i].symval, msl_eval(a1.values[i+1], let_env))
+                        env = let_env
+                        ast = a2
+
+                    elif funcname == 'exit' or funcname == 'quit':
+                        a1 = ast.values[1]
+                        if not isinstance(a1, mtypes.MslNumber):
+                            raise Exception("A number is required")
+                        sys.exit(a1.num)
+
+                    elif funcname == 'do':
+                        eval_ast(ast.values[1:-1], env)
+                        ast = ast[-1]
+
+                    elif funcname == 'if':
+                        a1 = ast.values[1]
+                        ret = msl_eval(a1, env)
+
+                        cond = msl_eval(a1, env)
+                        if cond is None or cond is False:
+                            if len(ast.values) > 3:
+                                ast = ast.values[3]
+                            else:
+                                ast = None
                         else:
-                            # just as usual, get from the env
-                            func_call = envfunc.get(funcname)
+                            ast = ast.values[2]
+
+                    elif funcname == 'fn*':
+                        a1, a2 = ast.values[1], ast.values[2]
+                        return mtypes.MslFunction(msl_eval, menv.Enviroment, a2, env, a1)
+
                     else:
-                        func_call = envfunc.get(funcname)
+                        d = eval_ast(ast, env)
+                        f = d[0]
 
-                    if func_call == None:
-                        raise Exception("No function %s found" % funcname)
+                        print(f)
 
-                    return func_call(*fargs)
+                        if hasattr(f, '__ast__'):
+                            print("got __ast__ func")
+                            ast = f.__ast__
+                            env = f.__gen__env(d.values[1:])
+                        else:
+                            return f(*d.values[1:])
+
+                        '''
+                        if hasattr(funcname, 'type'):
+                            if funcname.type == 'function':
+                                # function call(maybe?)
+                                func_call = funcname
+                            elif funcname.type == 'list':
+                                # got lambda
+                                func_call = msl_eval(funcname, env)
+                                func_type = 'decl'
+                            else:
+                                # just as usual, get from the env
+                                func_call = envfunc.get(funcname)
+                        else:
+                            func_call = envfunc.get(funcname)
+
+                        if func_call == None:
+                            raise Exception("No function %s found" % funcname)
+
+                        return func_call(*fargs)
+                        '''
+            else:
+                return eval_ast(ast, env)
         else:
             return eval_ast(ast, env)
-    else:
-        return eval_ast(ast, env)
-    return ast
+        return ast
 
 def msl_print(exp):
     return printer.pr_str(exp, True)
