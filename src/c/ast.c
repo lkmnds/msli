dbabf13226bd087980a474cee5c4f324bc2a2a8d
Luna Mendes <lkmnds@gmail.com>
2017-09-06 23:19:01 -0300

    move py/* to src

--- src/c/ast.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include "parser.h"
-#include "ast.h"
-
-msl_ast_t *msl_new_ast(msl_tokens_t *tokens){
-	msl_ast_t *ret = malloc(sizeof(msl_ast_t));
-	//ret->tokens = tokens;
-	return ret;
-}
--- src/c/ast.h
+++
@@ -1,9 +0,0 @@
-#ifndef __AST_H__
-#define __AST_H__
-
-typedef struct {
-	int b;
-	//msl_tokens_t *tokens;
-} msl_ast_t;
-
-#endif
