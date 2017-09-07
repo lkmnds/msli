49263274f2eea005dfe9293d9f52813d119a3494
Luna Mendes <lkmnds@gmail.com>
2017-09-06 23:14:56 -0300

    env: use merror.debug

--- src/py/msl_env.py
+++ src/py/msl_env.py
@@ -17,14 +17,14 @@ class Enviroment:
 
     def set(self, k, v):
         if isinstance(k, mtypes.MslSymbol):
-            print("warning: setting symbol %s as key" % k.symval)
+            merror.debug(f'warning: setting symbol {k.symval!r} as key')
             k = k.symval
         self.data[k] = v
         return v
 
     def find(self, symbol):
         if isinstance(symbol, mtypes.MslSymbol):
-            print("warning: finding a symbol %s" % symbol.symval)
+            merror.debug(f'warning: finding sym {symbol.symval!r}')
             symbol = symbol.symval
 
         # print("env: find %s (%s)" % (symbol, type(symbol)))
@@ -39,10 +39,11 @@ class Enviroment:
 
     def get(self, symbol):
         if isinstance(symbol, mtypes.MslSymbol):
-            print("warning: getting a symbol %s" % symbol.symval)
+            merror.debug(f'warning: getting a symbol {sumbol.symval!r}')
             symbol = symbol.symval
 
         env = self.find(symbol)
         if not env:
-            raise Exception("enviroment: Symbol %s not found" % symbol)
+            raise RuntimeError('environment: Symbol {symbol!r} not found')
+
         return env.data[symbol]
